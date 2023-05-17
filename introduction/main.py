8fb3c0e6b2546580a73f34b52d2f95d7963cb5e5
ujjwal <124008221+ujjwal-basnet@users.noreply.github.com>
2023-05-17 11:36:26 +0545

    app.rul.rule() function

--- introduction/main.py
+++ introduction/main.py
@@ -8,4 +8,15 @@ app = Flask(__name__)
 def hellow_world():
     return "Hellow world "
 
+
+#or you can also do 
+def h_world():
+    return "hellow world"
+app.add.url_rule('/', 'h' , h_world)
+
+#now run and do /h enter with exesting url 
+
+# to run 
+if __name__ == '__main__':
+    app.run()
 #about section 
