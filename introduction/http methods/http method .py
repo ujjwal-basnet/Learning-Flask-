319aa253d30c09a8ed4e016a159e2ed68f2a0409
ujjwal <124008221+ujjwal-basnet@users.noreply.github.com>
2023-05-17 14:59:19 +0545

    j

---
+++ introduction/http methods/http method .py
@@ -0,0 +1,5 @@
+#some are
+# get , head , post , put , delete 
+
+
+#By default, the Flask route responds to the GET requests. However, this preference can be altered by providing methods argument to route() decorator
